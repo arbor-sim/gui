297cd2e4b38c859259a6bf322d0d0afbea2e5a2a
Thorsten Hater <24411438+thorstenhater@users.noreply.github.com>
2025-03-10 15:05:37 +0100

    Fix ini, add last case to swc-inter

--- src/gui_state.cpp
+++ src/gui_state.cpp
@@ -289,7 +289,7 @@ namespace {
     // DockSpace
     ImGuiIO& io = ImGui::GetIO();
 
-    if (io.ConfigFlags&  ImGuiConfigFlags_DockingEnable) {
+    if (io.ConfigFlags & ImGuiConfigFlags_DockingEnable) {
       ImGuiID dockspace_id = ImGui::GetID("MyDockSpace");
       ImGui::DockSpace(dockspace_id, ImVec2(0.0f, 0.0f), dockspace_flags);
     }
